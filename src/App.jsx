09aa3aed5cdadd949a241254bed16e86256d99a7
joellekounde <djokounde@gmail.com>
2023-09-02 20:19:34 +0200

    React03  Challenge- Ajout function ternaire +Mew ???

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import './App.css'
 import Pokemoncard from './components/PokemonCard'
 
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function App() {
  
   return (
     <>
-      <Pokemoncard/>
+      <Pokemoncard pokemon={pokemonList[0]}/>
     </>
   )
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,9 +1,11 @@
-function Pokemoncard(){
+function Pokemoncard({pokemon}){
     return (
-        <figure>
-  <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-  <figcaption>Bulbasaur</figcaption>
-</figure>
-    )
+
+ <div>
+    <figure>{pokemon.imgSrc === undefined ?"???": <img src = {pokemon.imgSrc} alt= {pokemon.name}/>}
+      <figcaption>{pokemon.name}</figcaption>
+   </figure>
+</div>
+    );
 }
 export default Pokemoncard;
