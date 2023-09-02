399c3381cdc7adb6c8202a49112afa7a00ac2475
joellekounde <djokounde@gmail.com>
2023-09-02 22:57:41 +0200

    React 06 Challenge- Ajout des boutons précédent et suivant

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,48 @@
 import './App.css'
 import Pokemoncard from './components/PokemonCard'
-
+import { useState } from 'react';
 
 const pokemonList = [
   {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 function App() {
+  const [count,setcount] = useState(0)
+  const handlePreviousClick =() => {
+    setcount (count -1)
+  }
+  const handleNextClick=() => {
+    setcount (count +1)
+  }
  
   return (
     <>
-     <Pokemoncard {...pokemonList[0]}/>
-     <Pokemoncard {...pokemonList[1]}/>
+     <Pokemoncard {...pokemonList[count]}/>
+     <button onClick={handlePreviousClick}>Précédent</button>
+     <button onClick={handleNextClick}>Suivant</button>
+     
     </>
   )
 }
