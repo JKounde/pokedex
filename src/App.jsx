53e1f4c705c666b8eec634b206a691d82b106331
joellekounde <djokounde@gmail.com>
2023-09-16 09:13:51 +0200

    creation useEffect challenge React Basics 07bis

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Pokemoncard from './components/PokemonCard'
 import { useState } from 'react';
 import NavBar from './components/navbar';
+import { useEffect } from 'react';
 
 
 const pokemonList = [
@@ -33,17 +34,51 @@ const pokemonList = [
   
   function App() {
     const [pokemonIndex,setpokemonIndex] = useState(0)
+
+    useEffect(
+      () => {
+        alert( "Hello pokemon trainer :)"); 
+      },
+      []
+    );
   
-  
+/*{name === "pikachu"? useEffect(
+  () => {
+    alert( "pika pikachu !!!"); 
+  },
+  []
+);: <img src= {imgSrc}  alt= {name} />*/
+
+
+
+
+    useEffect(
+      () => {
+        alert( "Hello pokemon trainer :)"); 
+      },
+      []
+    );
+
+
     const handlePreviousClick =() => {
-      if(pokemonIndex > 0)
-      setpokemonIndex (pokemonIndex -1)
-    }
+      if(pokemonIndex > 0){
+     
+ const previousPokemon = pokemonList[pokemonIndex -1].name;
+  setpokemonIndex (pokemonIndex -1);
+
+ if (previousPokemon === "pikachu") {
+  alert("pika pikachu !!!");
+ }
+    }};
   
     const handleNextClick=() => {
-      if(pokemonIndex < pokemonList.length - 1)
-      setpokemonIndex (pokemonIndex +1)
-    }
+      if(pokemonIndex < pokemonList.length - 1){
+      const nextPokemon = pokemonList[pokemonIndex +1].name;
+      setpokemonIndex (pokemonIndex +1);
+      if (nextPokemon ==="pikachu") {
+        alert("pika pikachu !!!");  
+      }
+   }};
    
     return (
       <>
